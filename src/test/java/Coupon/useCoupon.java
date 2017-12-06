fde7592e8c73ec58d44fabc34723ab62dc08715d
Raul Arturo Dzul Rios <raul.dzul@4thsource.com>
2017-12-06 16:32:20 -0600

    Test 1

--- src/test/java/Coupon/useCoupon.java
+++ src/test/java/Coupon/useCoupon.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+// Prueba parar GitHub
 	@RunWith(Cucumber.class) 
 	@CucumberOptions(
 			features = "Features//Coupon//use_coupon.feature"
