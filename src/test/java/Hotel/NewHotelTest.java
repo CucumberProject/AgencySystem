0662f3fd76219d8be1aecf43338cd57677b2acd1
Aldair Cauich Chiu <aldair.cauich@4thsource.com>
2017-12-06 16:36:52 -0600

    Cometario de aldair prueba

--- src/test/java/Hotel/NewHotelTest.java
+++ src/test/java/Hotel/NewHotelTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
+//Comentario prueba de aldair
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"Features//Hotel//newhotel.feature"},
