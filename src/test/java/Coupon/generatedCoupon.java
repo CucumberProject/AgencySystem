9282c122d36edc420a76396a34f3404d4edbe35e
Raul Arturo Dzul Rios <raul.dzul@4thsource.com>
2017-12-06 17:11:09 -0600

    Generated Coupon Added

---
+++ src/test/java/Coupon/generatedCoupon.java
@@ -0,0 +1,12 @@
+package Coupon;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+	@RunWith(Cucumber.class) 
+	@CucumberOptions(
+			features = "Features//Coupon//generated_coupon.feature"
+			)
+	public class generatedCoupon {
+}
